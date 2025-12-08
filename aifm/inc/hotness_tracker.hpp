fa59d01fe49ded3c947adf81516ee4b5d8539b1c
Ahmed Malanovic <202331240+malano77@users.noreply.github.com>
2025-12-08 13:22:40 +0100

    Increase logging entries

--- aifm/inc/hotness_tracker.hpp
+++ aifm/inc/hotness_tracker.hpp
@@ -18,7 +18,7 @@ public:
   static void record(uint64_t object_id);
   static std::vector<Entry> snapshot();
   static void reset();
-  static void dump_top(const char *label, size_t max_entries = 8);
+  static void dump_top(const char *label, size_t max_entries = 16);
 
 private:
   struct ThreadLocalCounters {
