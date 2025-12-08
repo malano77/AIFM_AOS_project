801d9011c2493396fcfda746c406f3ade698c245
Ahmed Malanovic <202331240+malano77@users.noreply.github.com>
2025-12-08 17:20:50 +0100

    Adapt run script for fig 11 a snappy

--- aifm/exp/fig11a/aifm/run.sh
+++ aifm/exp/fig11a/aifm/run.sh
@@ -12,7 +12,6 @@ do
     make clean
     make -j
     rerun_local_iokerneld_noht
-    rerun_mem_server    
     run_program_noht ./main 1>log.$local_rams 2>&1 &
     ( tail -f -n0 log.$local_rams & ) | grep -q "Force existing..."
     sudo pkill -9 main
