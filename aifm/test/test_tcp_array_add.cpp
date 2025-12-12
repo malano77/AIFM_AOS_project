6ccf74990ad98c9bb54af0efea7a62a424dd3871
Ahmed Malanovic <202331240+malano77@users.noreply.github.com>
2025-12-12 10:21:57 +0100

    Add instrumentation for TCPDevice test case

--- aifm/test/test_tcp_array_add.cpp
+++ aifm/test/test_tcp_array_add.cpp
@@ -5,7 +5,9 @@ extern "C" {
 #include "array.hpp"
 #include "device.hpp"
 #include "helpers.hpp"
+#include "hotness_tracker.hpp"
 #include "manager.hpp"
+#include "stats.hpp"
 
 #include <cstdint>
 #include <cstring>
@@ -75,10 +77,26 @@ void do_work(FarMemManager *manager) {
   }
 
   cout << "Passed" << endl;
+  HotnessTracker::dump("test_tcp_array_add");
+  HotnessTracker::reset();
+#ifdef MONITOR_READ_OBJECT_CYCLES
+  std::cout << "[Stats] read_ops=" << Stats::get_num_read_object_ops()
+            << " total_read_cycles=" << Stats::get_total_read_object_cycles()
+            << " avg_read_cycles=" << Stats::get_avg_read_object_cycles()
+            << std::endl;
+#endif
+#ifdef MONITOR_WRITE_OBJECT_CYCLES
+  std::cout << "[Stats] write_ops=" << Stats::get_num_write_object_ops()
+            << " total_write_cycles=" << Stats::get_total_write_object_cycles()
+            << " avg_write_cycles=" << Stats::get_avg_write_object_cycles()
+            << std::endl;
+#endif
   return;
 
 fail:
   cout << "Failed" << endl;
+  HotnessTracker::dump("test_tcp_array_add");
+  HotnessTracker::reset();
 }
 
 int argc;
@@ -86,6 +104,8 @@ void _main(void *arg) {
   char **argv = static_cast<char **>(arg);
   std::string ip_addr_port(argv[1]);
   auto raddr = helpers::str_to_netaddr(ip_addr_port);
+  Stats::reset_read_object_cycle_stats();
+  Stats::reset_write_object_cycle_stats();
   std::unique_ptr<FarMemManager> manager =
       std::unique_ptr<FarMemManager>(FarMemManagerFactory::build(
           kCacheSize, kNumGCThreads,
